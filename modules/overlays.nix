a47fde239eec5f1dd5b60d9e7d751b2fd0462d00
Eric Dattore <eric@dattore.me>
2022-05-20 21:43:25 -0600

    Add overlay for aiohttp
    Add an overlay to patch out `trustme` from the `aiohttp` package and build on aarch64-darwin again.

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -4,5 +4,23 @@
       # expose stable packages via pkgs.stable
       stable = import inputs.stable { system = prev.system; };
     })
+    (self: super: {
+      python = super.python.override {
+        packageOverrides = self: super: {
+          aiohttp = super.aiohttp.overrideAttrs (old: {
+            checkInputs = [
+              async_generator
+              freezegun
+              gunicorn
+              pytest-mock
+              pytestCheckHook
+              re-assert
+            ] ++ lib.optionals stdenv.isLinux [
+              trustme
+            ];
+          };
+        };
+      };
+    })
   ];
 }
