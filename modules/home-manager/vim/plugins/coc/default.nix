2eb969121de8ac7880bfb159d514807827d45b7b
Eric Dattore <eric.dattore@gmail.com>
2022-05-21 16:58:12 -0600

    Patch python and neovim deps
    `pyopenssl` is broken on Apple Silicon and [this PR marks it as
    broken](https://github.com/NixOS/nixpkgs/pull/172397) (which I think is
    a little overzealous).
    [This other pending PR fixes the issue](https://github.com/NixOS/nixpkgs/pull/173809)
    by making `trustme` an optional dependency for `aiohttp`. While the PR
    is pending, I've added an overlay to accomplish the same thing — and
    mark `pyopenssl` as _not_ broken, for now.
    
    Additionally, Neovim broke in recent versions of home-manager, for some
    reason. There's a workaround available [here](https://github.com/nix-community/home-manager/issues/2966#issuecomment-1133667918)
    and the home-manager maintainers worked quickly to merge a fix in.
    
    This unblocks updating all flake dependencies and gets the system config
    back into working order.

--- modules/home-manager/vim/plugins/coc/default.nix
+++ modules/home-manager/vim/plugins/coc/default.nix
@@ -1,43 +1,111 @@
-{ config, pkgs, lib, ... }: {
-  # link coc-settings to the right location
-  xdg.configFile."nvim/coc-settings.json".source = ./coc-settings.json;
-
+{ pkgs, ... }: {
   programs.neovim =
     {
-      extraPackages = with pkgs; [
-        rubyPackages.solargraph
-        nodePackages.pyright
-        rnix-lsp
-        fzf
-      ];
-      plugins = with pkgs.vimPlugins; [
-        (config.lib.vimUtils.pluginWithCfg {
-          plugin = coc-nvim;
-          file = ./coc-nvim.vim;
-        })
-        coc-css
-        coc-eslint
-        coc-fzf
-        coc-git
-        coc-go
-        coc-html
-        coc-java
-        coc-json
-        coc-lua
-        coc-pairs
-        coc-prettier
-        coc-pyright # python
-        coc-r-lsp
-        coc-rls
-        coc-smartf
-        coc-snippets
-        coc-solargraph
-        coc-tslint
-        coc-tsserver # js/ts
-        coc-vetur # vuejs
-        coc-vimlsp # vimL
-        coc-vimtex # latex
-        coc-yaml
-      ];
+      coc = {
+        enable = true;
+
+        package = pkgs.vimUtils.buildVimPluginFrom2Nix
+          {
+            pname = "coc.nvim";
+            version = "2022-05-21";
+            src = pkgs.fetchFromGitHub {
+              owner = "neoclide";
+              repo = "coc.nvim";
+              rev = "791c9f673b882768486450e73d8bda10e391401d";
+              sha256 = "sha256-MobgwhFQ1Ld7pFknsurSFAsN5v+vGbEFojTAYD/kI9c=";
+            };
+            meta.homepage = "https://github.com/neoclide/coc.nvim/";
+          };
+
+        settings = {
+          "coc.preferences.rootPatterns" = [ ".git" ];
+          "explorer.keyMappings.global" = {
+            "<cr>" = [ "expandable?" [ "expanded?" "collapse" "expand" ] "open" ];
+          };
+
+          "explorer.file.showHiddenFiles" = true;
+          "explorer.git.icon.status.added" = "✚";
+          "explorer.git.icon.status.copied" = "➜";
+          "explorer.git.icon.status.deleted" = "✖";
+          "explorer.git.icon.status.ignored" = "☒";
+          "explorer.git.icon.status.mixed" = "✹";
+          "explorer.git.icon.status.modified" = "✹";
+          "explorer.git.icon.status.renamed" = "➜";
+          "explorer.git.icon.status.unmerged" = "═";
+          "explorer.git.icon.status.untracked" = "?";
+          "explorer.git.showIgnored" = true;
+          "explorer.icon.enableNerdfont" = true;
+          "explorer.position" = "right";
+
+          "languageserver" = {
+            "nix" = {
+              "command" = "rnix-lsp";
+              "filetypes" = [ "nix" ];
+            };
+          };
+        };
+
+        pluginConfig = ''
+          " CoC Extensions
+          let g:coc_global_extensions=[
+              \ 'coc-clangd',
+              \ 'coc-css',
+              \ 'coc-dictionary',
+              \ 'coc-docker',
+              \ 'coc-elixir',
+              \ 'coc-eslint',
+              \ 'coc-explorer',
+              \ 'coc-go',
+              \ 'coc-html',
+              \ 'coc-java',
+              \ 'coc-java-debug',
+              \ 'coc-json',
+              \ 'coc-kotlin',
+              \ 'coc-lists',
+              \ 'coc-marketplace',
+              \ 'coc-omnisharp',
+              \ 'coc-prettier',
+              \ 'coc-pyright',
+              \ 'coc-rust-analyzer',
+              \ 'coc-sh',
+              \ 'coc-texlab',
+              \ 'coc-tsserver',
+              \ 'coc-word',
+              \ 'coc-yaml',
+              \ ]
+          set updatetime=300
+          set shortmess+=c
+          " Use tab for trigger completion with characters ahead and navigate.
+          inoremap <silent><expr> <TAB>
+                \ pumvisible() ? "\<C-n>" :
+                \ <SID>check_back_space() ? "\<TAB>" :
+                \ coc#refresh()
+          inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+          function! s:check_back_space() abort
+            let col = col('.') - 1
+            return !col || getline('.')[col - 1]  =~# '\s'
+          endfunction
+          " Remap keys for gotos
+          nmap <silent> gd <Plug>(coc-definition)
+          nmap <silent> gy <Plug>(coc-type-definition)
+          nmap <silent> gi <Plug>(coc-implementation)
+          nmap <silent> gr <Plug>(coc-references)
+          " Formatting
+          vmap <C-F> <Plug>(coc-format-selected)
+          xmap <C-F> :call CocAction('format')<CR>
+          nmap <C-F> :call CocAction('format')<CR>
+          " Hover and rename
+          nmap <silent> <F6> <Plug>(coc-rename)
+          nnoremap <silent> K :call CocAction('doHover')<CR>
+          " Go to symbol in (document|project)
+          nmap <silent> S :CocList symbols<CR>
+          " CoC Explorer
+          function! CocExploreCwd()
+              let cwd = substitute(execute(":pwd"), '\n', "", "")
+              exe 'CocCommand explorer ' . cwd
+          endfunction
+          map <S-T> :call CocExploreCwd()<CR>
+        '';
+      };
     };
 }
--- modules/overlays.nix
+++ modules/overlays.nix
@@ -1,8 +1,27 @@
-{ inputs, nixpkgs, stable, ... }: {
+{ inputs
+, nixpkgs
+, stable
+, ...
+}: {
   nixpkgs.overlays = [
     (final: prev: {
       # expose stable packages via pkgs.stable
       stable = import inputs.stable { system = prev.system; };
+    })
+    (final: prev: rec {
+      python3 = prev.python3.override
+        {
+          packageOverrides = final: prev: {
+            aiohttp = prev.aiohttp.overrideAttrs
+              (old: {
+                checkInputs = builtins.filter (pkg: pkg != inputs.trustme) old.checkInputs;
+              });
+            pyopenssl = prev.pyopenssl.overrideAttrs
+              (old: {
+                meta = old.meta // { broken = false; };
+              });
+          };
+        };
     })
   ];
 }
