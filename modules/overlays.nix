5746f952d2e8b8c21df41a9e1ea84723593c484e
Eric Dattore <eric@dattore.me>
2022-06-26 18:22:19 -0600

    Update Flake Inputs (#38)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'devshell':
        'github:numtide/devshell/f55e05c6d3bbe9acc7363bc8fc739518b2f02976' (2022-06-10)
      → 'github:numtide/devshell/899ca4629020592a13a46783587f6e674179d1db' (2022-06-23)
    • Updated input 'flake-utils':
        'github:numtide/flake-utils/1ed9fb1935d260de5fe1c2f7ee0ebaae17ed2fa1' (2022-05-30)
      → 'github:numtide/flake-utils/bee6a7250dd1b01844a2de7e02e4df7d8a0a206c' (2022-06-24)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/e622bad16372aa5ada79a7fa749ec78715dffc54' (2022-06-22)
      → 'github:nix-community/home-manager/1dbac84b846e4bfa21a08e31e95e11f0965ed042' (2022-06-25)
    • Updated input 'nixos-unstable':
        'github:nixos/nixpkgs/e1e08fe28bf0588a41cd556eac40b98d2793da99' (2022-06-22)
      → 'github:nixos/nixpkgs/6141b8932a5cf376fe18fcd368cecd9ad946cb68' (2022-06-23)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/e0a42267f73ea52adc061a64650fddc59906fc99' (2022-06-18)
      → 'github:nixos/nixpkgs/72d1b0d0fac131df1ea254b65413c85609bdd2ee' (2022-06-25)
    • Updated input 'stable':
        'github:nixos/nixpkgs/63198c9ccefdbd337cef0d85db0ea2689f4ce418' (2022-06-21)
      → 'github:nixos/nixpkgs/902d91def1efbea804f5158e5999cb113cedf04b' (2022-06-24)
    
    * Update for new home-manager config
    
    Co-authored-by: flakebot <flakebot@users.noreply.github.com>

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -7,21 +7,7 @@
     (final: prev: {
       # expose stable packages via pkgs.stable
       stable = import inputs.stable { system = prev.system; };
-    })
-    (final: prev: rec {
-      python3 = prev.python3.override
-        {
-          packageOverrides = final: prev: {
-            aiohttp = prev.aiohttp.overrideAttrs
-              (old: {
-                checkInputs = builtins.filter (pkg: pkg != inputs.trustme) old.checkInputs;
-              });
-            pyopenssl = prev.pyopenssl.overrideAttrs
-              (old: {
-                meta = old.meta // { broken = false; };
-              });
-          };
-        };
+      small = import inputs.stable { system = prev.system; };
     })
   ];
 }
