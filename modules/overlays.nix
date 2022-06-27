1bbea7b207f04eadaf81dffa514a171999bffdb8
Eric Dattore <eric.dattore@circleci.com>
2022-06-27 01:06:56 -0600

    Patch python39 and python310 pyopenssl

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -9,5 +9,27 @@
       stable = import inputs.stable { system = prev.system; };
       small = import inputs.stable { system = prev.system; };
     })
+    (final: prev: {
+      python39 = prev.python39.override {
+        packageOverrides = pfinal: pprev: {
+          pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
+            meta = with old; {
+              inherit meta;
+              broken = false;
+            };
+          });
+        };
+      };
+      python310 = prev.python39.override {
+        packageOverrides = pfinal: pprev: {
+          pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
+            meta = with old; {
+              inherit meta;
+              broken = false;
+            };
+          });
+        };
+      };
+    })
   ];
 }
