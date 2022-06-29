ef99f55b54cc036d7988f58fe43d7afcc5d7abd8
Eric Dattore <eric@dattore.me>
2022-06-29 10:58:12 -0600

    Update Flake Inputs (#39)
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/1dbac84b846e4bfa21a08e31e95e11f0965ed042' (2022-06-25)
      → 'github:nix-community/home-manager/3bf16c0fd141c28312be52945d1543f9ce557bb1' (2022-06-27)
    • Removed input 'home-manager/flake-compat'
    • Removed input 'home-manager/nmd'
    • Removed input 'home-manager/nmt'
    • Updated input 'nixos-hardware':
        'github:nixos/nixos-hardware/f9d8dff4e621f2d7f2b84d9e84bc6359715f971c' (2022-06-21)
      → 'github:nixos/nixos-hardware/78e7c2c397b0376526e83162b58de921362e3399' (2022-06-27)
    • Updated input 'nixos-unstable':
        'github:nixos/nixpkgs/6141b8932a5cf376fe18fcd368cecd9ad946cb68' (2022-06-23)
      → 'github:nixos/nixpkgs/f2537a505d45c31fe5d9c27ea9829b6f4c4e6ac5' (2022-06-26)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/72d1b0d0fac131df1ea254b65413c85609bdd2ee' (2022-06-25)
      → 'github:nixos/nixpkgs/0c4852c7bc40747e734a84a0d234105a4d5c146f' (2022-06-27)
    • Updated input 'small':
        'github:nixos/nixpkgs/60801d44897a49a07190dee82ddd2d05192bd057' (2022-06-25)
      → 'github:nixos/nixpkgs/0c4852c7bc40747e734a84a0d234105a4d5c146f' (2022-06-27)
    • Updated input 'stable':
        'github:nixos/nixpkgs/902d91def1efbea804f5158e5999cb113cedf04b' (2022-06-24)
      → 'github:nixos/nixpkgs/891016b5cf1998ea84852adac52c65c5ccd3e802' (2022-06-25)
    
    * fix pyopenssl, again
    
    * Update flake.lock
    
    Co-authored-by: flakebot <flakebot@users.noreply.github.com>

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -9,5 +9,21 @@
       stable = import inputs.stable { system = prev.system; };
       small = import inputs.stable { system = prev.system; };
     })
+    (final: prev: {
+      python310 = prev.python310.override {
+        packageOverrides = (pfinal: pprev: {
+          pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
+            meta = old.meta // { broken = false; };
+          });
+        });
+      };
+      python39 = prev.python39.override {
+        packageOverrides = (pfinal: pprev: {
+          pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
+            meta = old.meta // { broken = false; };
+          });
+        });
+      };
+    })
   ];
 }
