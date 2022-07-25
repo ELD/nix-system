624de7413ca24eb44e373df570c56cb8d623a63e
Eric Dattore <eric.dattore@circleci.com>
2022-07-25 11:01:32 -0600

    Patch all of the Python3 pyopenssl packages

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -10,6 +10,13 @@
       small = import inputs.stable { system = prev.system; };
     })
     (final: prev: {
+      python3 = prev.python3.override {
+        packageOverrides = (pfinal: pprev: {
+          pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
+            meta = old.meta // { broken = false; };
+          });
+        });
+      };
       python310 = prev.python310.override {
         packageOverrides = (pfinal: pprev: {
           pyopenssl = pprev.pyopenssl.overrideAttrs (old: {
