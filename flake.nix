072b7c587a3f76c9afe807e2ce1162af8a64ddaa
Eric Dattore <eric.dattore@gmail.com>
2022-10-15 14:45:51 -0600

    Remove circleci-cli overlay

--- flake.nix
+++ flake.nix
@@ -292,25 +292,6 @@
               packageOverrides = overrides;
             };
           };
-        circleci-cli = final: prev: {
-          circleci-cli =
-            let
-              version = "0.1.20144";
-              pname = "circleci";
-              src = final.pkgs.fetchFromGitHub {
-                owner = "CircleCI-Public";
-                repo = "${pname}-cli";
-                rev = "v${version}";
-                sha256 = "sha256-69GGJfnOHry+N3hKZapKz6eFSerqIHt4wRAhm/q/SOQ=";
-              };
-            in
-            (prev.circleci-cli.override {
-              buildGoModule = args: final.pkgs.buildGoModule.override { go = final.pkgs.go_1_17; } (args // {
-                inherit src version;
-                vendorSha256 = "sha256-7u2y1yBVpXf+D19tslD4s3B1KmABl4OWNzzLaBNL/2U=";
-              });
-            });
-        };
         sbctl = final: prev: {
           sbctl = final.callPackage ./modules/packages/sbctl.nix { };
         };
