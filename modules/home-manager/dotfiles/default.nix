7fdc2db01788085b97c81513cf974936d4018252
Eric Dattore <eric@dattore.me>
2022-10-28 14:35:22 -0600

    change iterm2 pref linking

--- modules/home-manager/dotfiles/default.nix
+++ modules/home-manager/dotfiles/default.nix
@@ -1,8 +1,5 @@
-{ config, pkgs, lib, ... }:
-let
-  link = config.lib.file.mkOutOfStoreSymlink;
-in {
+{ config, pkgs, lib, ... }: {
   xdg.configFile = { } // (lib.optionalAttrs pkgs.stdenv.isDarwin {
-    "iterm2/com.googlecode.iterm2.plist".source = link ./. + "/iterm2/com.googlecode.iterm2.plist";
+    "iterm2/com.googlecode.iterm2.plist".source = ./iterm2/com.googlecode.iterm2.plist;
   });
 }
