d2ecceab6a4b037cd67d51ef14d77f41325f5e65
Eric Dattore <eric@dattore.me>
2023-01-03 23:06:21 -0700

    Comment out mapleader for NeoVim
    Because of order(?), it seems remapping the leader key doesn't work for
    all plugins in NeoVim. Not sure what's going on here, but it needs more
    debugging for now.

--- modules/home-manager/nvim/keybindings.lua
+++ modules/home-manager/nvim/keybindings.lua
@@ -1,5 +1,6 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- Can't figure out how to remap the leader key for now, so let's just not rebind it...
+-- vim.g.mapleader = ' '
+-- vim.g.maplocalleader = ' '
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
