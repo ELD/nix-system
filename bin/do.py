7fcb25243819a0a0b12b0d5593bff30415288d59
Eric Dattore <eric@dattore.me>
2023-02-17 08:57:32 -0700

    Update `bin/do.py`

--- bin/do.py
+++ bin/do.py
@@ -23,7 +23,7 @@ class Colors(Enum):
 
 check_git = subprocess.run(["git", "rev-parse", "--show-toplevel"], capture_output=True)
 LOCAL_FLAKE = os.path.realpath(check_git.stdout.decode().strip())
-REMOTE_FLAKE = "github:kclejeune/system"
+REMOTE_FLAKE = "github:ELD/nix-system"
 is_local = check_git.returncode == 0 and os.path.isfile(
     os.path.join(LOCAL_FLAKE, "flake.nix")
 )
@@ -315,7 +315,7 @@ def switch(
 
 
 @app.command(hidden=not is_local, help="cache the output environment of flake.nix")
-def cache(cache_name: str = "kclejeune"):
+def cache(cache_name: str = "eld"):
     cmd = f"nix flake archive --json | jq -r '.path,(.inputs|to_entries[].value.path)' | cachix push {cache_name}"
     run_cmd(cmd.split(), shell=True)
 
