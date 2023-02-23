14f802bbfb27b3cf8d2661b6d4eb6f34414b1b12
Eric Dattore <eric.dattore@circleci.com>
2023-02-23 12:04:30 -0700

    untap `homebrew/core` and `homebrew/cask`
    Due to changes in Homebrew 4, the taps are no longer necessary because
    they download a JSON manifest

--- modules/darwin/brew.nix
+++ modules/darwin/brew.nix
@@ -16,11 +16,9 @@
 
     taps = [
       "homebrew/bundle"
-      "homebrew/cask"
       "homebrew/cask-drivers"
       "homebrew/cask-fonts"
       "homebrew/cask-versions"
-      "homebrew/core"
       "homebrew/services"
       "koekeishiya/formulae"
       "teamookla/speedtest"
