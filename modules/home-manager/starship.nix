66e76db8e84af41db4d6714c97bbbc02c64fa4c9
Eric Dattore <eric.dattore@circleci.com>
2023-06-23 10:21:32 -0600

    Switch to different prompt theme

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -3,106 +3,101 @@
     enable = true;
     settings = {
       format = lib.concatStrings [
-        "[](#9A348E)"
-        "$os"
-        "$username"
-        "[](bg:#DA627D fg:#9A348E)"
+        "[░▒▓](#A3AED2)"
+        "[  ](bg:#A3AED2 fg:#090c0c)"
+        "[](bg:#769FF0 fg:#A3AED2)"
         "$directory"
-        "[](fg:#DA627D bg:#FCA17D)"
+        "[](fg:#769FF0 bg:#394260)"
         "$git_branch"
         "$git_status"
-        "[](fg:#FCA17D bg:#86BBD8)"
-        "$c"
-        "$elixir"
-        "$elm"
-        "$golang"
-        "$gradle"
-        "$haskell"
-        "$java"
-        "$julia"
+        "[](fg:#394260 bg:#212736)"
         "$nodejs"
-        "$nim"
         "$rust"
-        "$scala"
-        "[](fg:#86BBD8 bg:#06969A)"
-        "$docker_context"
-        "[](fg:#06969A bg:#33658A)"
+        "$golang"
+        "$php"
+        "[](fg:#212736 bg:#1D2230)"
         "$time"
-        "[ ](fg:#33658A)"
+        "[ ](fg:#1D2230)"
       ];
 
-      username = {
-        show_always = true;
-        style_user = "bg:#9A348E";
-        style_root = "bg:#9A348E";
-        format = "[$user ]($style)";
-        disabled = false;
-      };
-
-      os = {
-        style = "bg:#9A348E";
-        disabled = true; # Disabled by default
-      };
-
       directory = {
-        style = "bg:#DA627D";
+        style = "fg:#E3E5E5 bg:#769FF0";
         format = "[ $path ]($style)";
         truncation_length = 3;
         truncation_symbol = "…/";
         substitutions = {
-          "Documents" = " ";
+          "Documents" = "󰈙 ";
           "Downloads" = " ";
           "Music" = " ";
           "Pictures" = " ";
         };
       };
 
-      docker_context = {
-        symbol = " ";
-        style = "bg:#06969A";
-        format = "[ $symbol $context ]($style)$path";
-      };
-
       git_branch = {
         symbol = "";
-        style = "bg:#FCA17D";
-        format = "[ $symbol $branch ]($style)";
+        style = "bg:#394260";
+        format = "[[ $symbol $branch ](fg:#769FF0 bg:#394260)]($style)";
       };
 
       git_status = {
-        style = "bg:#FCA17D";
-        format = "[$all_status$ahead_behind ]($style)";
-      };
-
-      golang = {
-        symbol = " ";
-        style = "bg:#86BBD8";
-        format = "[ $symbol ($version) ]($style)";
-      };
-
-      haskell = {
-        symbol = " ";
-        style = "bg:#86BBD8";
-        format = "[ $symbol ($version) ]($style)";
+        style = "bg:#394260";
+        format = "[[($all_status$ahead_behind )](fg:#769FF0 bg:#394260)]($style)";
       };
 
       nodejs = {
         symbol = "";
-        style = "bg:#86BBD8";
-        format = "[ $symbol ($version) ]($style)";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769FF0 bg:#212736)]($style)";
       };
 
       rust = {
         symbol = "";
-        style = "bg:#86BBD8";
-        format = "[ $symbol ($version) ]($style)";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769FF0 bg:#212736)]($style)";
+      };
+
+      golang = {
+        symbol = " ";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769FF0 bg:#212736)]($style)";
+      };
+
+      php = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769FF0 bg:#212736)]($style)";
       };
 
       time = {
         disabled = false;
         time_format = "%R"; # Hour:Minute Format
-        style = "bg:#33658A";
-        format = "[ ♥ $time ]($style)";
+        style = "bg:#1D2230";
+        format = "[[  $time ](fg:#a0a9cb bg:#1D2230)]($style)";
+      };
+
+      username = {
+        show_always = true;
+        style_user = "fg:#769FF0 bg:#212736";
+        style_root = "fg:#769FF0 bg:#212736";
+        format = "[$user ]($style)";
+        disabled = false;
+      };
+
+      os = {
+        style = "fg:#769FF0 bg:#212736";
+        disabled = true; # Disabled by default
+      };
+
+      docker_context = {
+        symbol = " ";
+        style = "fg:#769FF0 bg:#212736";
+        format = "[ $symbol $context ]($style)$path";
+      };
+
+      haskell = {
+        symbol = " ";
+        style = "fg:#769FF0 bg:#212736";
+        format = "[ $symbol ($version) ]($style)";
       };
     };
   };
