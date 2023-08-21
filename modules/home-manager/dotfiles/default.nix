12ee31bff03b094183236f8be4b1050de0b8e187
Eric Dattore <eric.dattore@circleci.com>
2023-08-21 15:10:54 -0500

    Manage prompt configuration in home-manager

--- modules/home-manager/dotfiles/default.nix
+++ modules/home-manager/dotfiles/default.nix
@@ -6,19 +6,23 @@
 }: let
   mkFullPathRelativeToNixpkgs = relative: "${config.home.homeDirectory}/.nixpkgs/${relative}";
 in {
+  home.file."${config.xdg.configHome}/zsh/.p10k.zsh" = {
+    source =
+      config.lib.file.mkOutOfStoreSymlink (mkFullPathRelativeToNixpkgs
+        "modules/home-manager/dotfiles/p10k/.p10k.zsh");
+  };
   xdg.configFile =
-    {}
-    // (lib.optionalAttrs pkgs.stdenv.isDarwin {
-      "iterm2/com.googlecode.iterm2.plist".source =
-        config.lib.file.mkOutOfStoreSymlink (mkFullPathRelativeToNixpkgs
-          "modules/home-manager/dotfiles/iterm2/com.googlecode.iterm2.plist");
-    })
-    // {
+    {
       nvim = {
         source =
           config.lib.file.mkOutOfStoreSymlink (mkFullPathRelativeToNixpkgs
             "modules/home-manager/dotfiles/spartanvim");
         recursive = true;
       };
-    };
+    }
+    // (lib.optionalAttrs pkgs.stdenv.isDarwin {
+      "iterm2/com.googlecode.iterm2.plist".source =
+        config.lib.file.mkOutOfStoreSymlink (mkFullPathRelativeToNixpkgs
+          "modules/home-manager/dotfiles/iterm2/com.googlecode.iterm2.plist");
+    });
 }
