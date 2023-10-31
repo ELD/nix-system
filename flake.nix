4934750590bb8b5c3b78f36b3a66e28ce45e6b08
Eric Dattore <eric@dattore.me>
2023-10-31 15:50:05 -0400

    switch to neovim-nightly-overlay from neovim-flake

--- flake.nix
+++ flake.nix
@@ -19,9 +19,10 @@
     stable.url = "github:nixos/nixpkgs/nixos-22.11";
     devenv.url = "github:cachix/devenv/v0.6.2";
 
-    neovim-flake = {
-      url = "github:neovim/neovim?dir=contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-staging.url = "github:nixos/nixpkgs/staging";
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs-staging";
     };
 
     # system management
@@ -326,18 +327,7 @@
       pop-launcher = final: _prev: {
         pop-launcher = final.callPackage ./modules/packages/pop-launcher.nix {};
       };
-      neovim = final: _prev: let
-        inherit (inputs.neovim-flake) packages;
-        neovim =
-          if isDarwin final.system
-          then packages.${final.system}.neovim
-          else packages.${final.system}.neovim;
-      in {
-        neovim-nightly =
-          if isDarwin final.system
-          then final.callPackage ./modules/packages/neovim-nightly.nix {inherit neovim;}
-          else neovim;
-      };
+      neovim = inputs.neovim-nightly-overlay.overlay;
       ripgrep-all = _final: prev: {
         ripgrep-all = prev.ripgrep-all.overrideAttrs (_old: {
           patches = [./modules/packages/ripgrep-all.patch];
