8fd5a7c06b98eb1b1130f25ca0fe10b3a6e441fc
Eric Dattore <eric@dattore.me>
2023-11-23 13:11:55 -0500

    Use GitHub Monaspace font (Monispace NFM Propo)

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -8,6 +8,7 @@ _: {
     shellIntegration.enableZshIntegration = true;
     theme = "Catppuccin-Macchiato";
     settings = {
+      font_features = "MonispaceNeNFM-Regular +calt +liga +dlig +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss08";
       tab_bar_min_tabs = 1;
       tab_bar_edge = "bottom";
       tab_bar_style = "powerline";
