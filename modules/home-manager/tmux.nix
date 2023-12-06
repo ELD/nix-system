bb7ad4f15558411cb1723aad77c90e621ef71d10
Eric Dattore <eric@dattore.me>
2023-12-06 16:42:44 -0700

    Update tmux configuration

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,5 +1,19 @@
-_: {
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
+    baseIndex = 1;
+    clock24 = true;
+    historyLimit = 10000;
+    keyMode = "vi";
+    mouse = true;
+    newSession = true;
+    plugins = [];
+    prefix = "C-a";
+    shell = "${pkgs.zsh}/bin/zsh";
+    terminal = "xterm-256color";
+    tmuxinator.enable = true;
+    extraConfig = ''
+      bind -n S-k clear-history
+    '';
   };
 }
