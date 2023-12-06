d9239806261dbe08e2c3ff42f508b471ab1fe328
Eric Dattore <eric.dattore@circleci.com>
2023-12-06 10:24:51 -0700

    Use release of Monaspice Rn font from GitHub

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -2,13 +2,13 @@ _: {
   programs.kitty = {
     enable = true;
     font = {
-      name = "MonispaceNE NFM";
+      name = "MonaspiceRnNFM-Regular";
       size = 16;
     };
     shellIntegration.enableZshIntegration = true;
     theme = "Catppuccin-Macchiato";
     settings = {
-      font_features = "MonispaceNeNFM-Regular +calt +liga +dlig +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss08";
+      font_features = "MonaspiceRnNFM-Regular +calt +liga +dlig +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss08";
       tab_bar_min_tabs = 1;
       tab_bar_edge = "bottom";
       tab_bar_style = "powerline";
