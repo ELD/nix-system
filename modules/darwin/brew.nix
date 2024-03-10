d34b0230c806452a2c6ce806ad2d46ce9f92f23b
Eric Dattore <eric@dattore.me>
2024-03-09 17:00:24 -0700

    Updates

--- modules/darwin/brew.nix
+++ modules/darwin/brew.nix
@@ -53,8 +53,10 @@
         "istat-menus"
         "iterm2-beta"
         "jetbrains-toolbox"
+        "kitty"
         "lunar"
         "macfuse"
+        "neovide"
         "numi"
         "obsidian"
         "orbstack" # Docker Desktop replacement?
