a810ae786e4cd465f89fdfe4282a3a2ee2b45a78
Eric Dattore <eric@dattore.me>
2024-04-05 09:22:00 -0600

    Update terminal configurations

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -2,26 +2,23 @@ _: {
   programs.kitty = {
     enable = true;
     font = {
-      name = "OperatorMonoSSmLig Nerd Font Regular";
+      name = "OperatorMonoSSmLig Nerd Font Mono";
       size = 18;
     };
     shellIntegration.enableZshIntegration = true;
     theme = "Catppuccin-Macchiato";
+    extraConfig = builtins.concatStringsSep "\n" [
+      "font_features OperatorMonoSSmLigNFM +ss01 +ss03 +ss05 +ss08 +ss09 +ss10 +ss11 +ss12"
+      "font_features OperatorMonoSSmLigNFM-Bold +ss01 +ss03 +ss05 +ss08 +ss09 +ss10 +ss11 +ss12"
+      "font_features OperatorMonoSSmLigNFM-Italic +ss01 +ss03 +ss05 +ss08 +ss09 +ss10 +ss11 +ss12"
+      "font_features OperatorMonoSSmLigNFM-BoldItalic +ss01 +ss03 +ss05 +ss08 +ss09 +ss10 +ss11 +ss12"
+    ];
     settings = {
-      bold_font = "OperatorMonoSSmLig Nerd Font Bold";
-      italic_font = "OperatorMonoSSmLig Nerd Font Italic";
-      bold_italic_font = "OperatorMonoSSmLig Nerd Font Bold Italic";
       tab_bar_min_tabs = 1;
       tab_bar_edge = "bottom";
       tab_bar_style = "powerline";
       tab_powerline_style = "slanted";
       tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
-      extra_config = ''
-        "OperatorMonoSSmLigNFM +calt +lig +dlig +ss01 +ss02 +ss03 +ss05 +ss06 +ss08 +ss12"
-        "OperatorMonoSSmLigNFM-Bold +calt +lig +dlig +ss01 +ss02 +ss03 +ss05 +ss06 +ss08 +ss12"
-        "OperatorMonoSSmLigNFM-Italic +calt +lig +dlig +ss01 +ss02 +ss03 +ss05 +ss06 +ss08 +ss12"
-        "OperatorMonoSSmLigNFM-BoldItalic +calt +lig +dlig +ss01 +ss02 +ss03 +ss05 +ss06 +ss08 +ss12"
-      '';
     };
   };
 }
