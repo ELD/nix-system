b797b0a499f5a770c3336718e5d92e3baf9011b5
Eric Dattore <eric@dattore.me>
2024-07-15 21:16:17 -0600

    Remove deprecated/removed `sound` option for NixOS

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -71,7 +71,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
